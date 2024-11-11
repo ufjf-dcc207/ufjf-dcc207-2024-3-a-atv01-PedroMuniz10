f3ff78b2333fdf24ee09b5e22cdacf2fbb96d5f1
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2024-11-11 20:51:11 -0300

    desestrutura props

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <>
       <h1>Olá mundo!</h1>
-      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
-      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
       <Disciplina codigo="DCC202" nome="Desenvolvimento Web I"/>
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
     </>
   );
 }
